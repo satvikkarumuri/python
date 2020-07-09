e08639508b26bb343e664475fb70feead5d30fbe
Satvik Karumuri <KarumuriSatvik@gmail.com>
2020-07-09 15:35:32 -0500

    Cleanup

---
+++ main.py
@@ -0,0 +1,123 @@
+# def test(t, t2):
+#     print(t)
+#     print(t2)
+# test(20, "test2")
+
+# c = 1
+# d = 1
+# if c != d:
+#     print("nope")
+# elif c == 1:
+#     print("its 1")
+# elif c == d:
+#     print("yes")
+# else:
+#     print("this will never happen")
+
+# def hi(a, b, c, d,):
+#     print(a)
+#     print(b)
+#     print(c)
+#     print(d)
+# hi("a","b","c","d")
+
+
+# def sayHi(name):
+#     print("Hi " + name)
+
+# # this is something
+# val = input("enter your name ") 
+# sayHi(val) 
+
+# sayHi("Bob")
+# sayHi("Billy")
+# sayHi("You")
+# sayHi("Me")
+# sayHi("Everyone")
+
+# list = [1, 2, 4, 9]
+# print(1, 2, 4, 9)
+    
+# a=1
+# b=2
+# d=4
+# j=9
+
+# if 1+2+4+9==15:
+#     print("sooo close")
+# elif 1+2+4+9==17:
+#     print("soooo close again")
+# elif 1+0+0+0==1:
+#     print ("not so smart, are you")
+# elif 1+2+4+9==16:
+#     print("yup")
+
+# def add(a, b):
+#     return a + b
+
+# print(add(1, 2))
+
+# This function subtracts two numbers
+# def subtract(j, d):
+#     return j - d 
+# print(subtract(9, 4))
+
+# a = input("Type in a number: ")
+# print(a)
+
+# b = input("b is the letter")
+# print(b)
+
+# number: integer
+# decimal: float
+# a = int(input("Type in your first number: "))
+# b = int(input("Type in your second number: "))
+# print(a + b)
+
+# asd = str(2.1)
+# asf = str(2.1)
+# print(asd + asf)
+
+# c = float(input("type the decimal:"))
+# d = float(input("type the second decimal:" ))
+# print(c*d)
+
+# abc = str(31.111)
+# abcd = str(31311.1)
+# print(abc + abcd)
+
+# def printHello():
+#     print("hello")
+
+# printHello()
+
+# num = int(input("Type in a number: "))
+# print("Your number is: %f jkjladsdfsg" % num)
+
+# num = str(input("type a name:"))
+# print("hello " + num)
+
+
+# c = int (input("what number "))
+# d = int (input("what number "))
+# a = input("add or subtract ")
+
+# if a == "add":
+#     print(c+d)
+# elif a == "subtract":
+#     print(c-d)
+
+def fibonacci:
+    a=0
+    b=1
+    if n == 0:
+        return a
+    elif n ==1:
+        return b
+    else:
+        for range:
+            c = a + b 
+            a = b
+            b = c
+        return b
+print(fibonacci)
