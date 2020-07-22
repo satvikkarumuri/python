188bc42a65a63939950efbff5cabf29ea9f8701b
Satvik.Karumuri <Kalyan.Karumuri@footlocker.com>
2020-07-21 19:44:48 -0500

    encode word

---
+++ encode.py
@@ -0,0 +1,17 @@
+# Take input
+name = input("type in a sercet word ")
+name = name.upper()
+
+output = " "
+
+# Loop word and write logic
+for letter in name:
+    a_value = ord(letter) + 1
+    if a_value > 90:
+        a_value -= 26
+    
+    new_letter = chr(a_value)
+    output += new_letter
+
+# Print result
+print(output)
