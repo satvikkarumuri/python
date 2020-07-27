59741315a471e003980962997010ede9f5e05423
Satvik.Karumuri <Kalyan.Karumuri@footlocker.com>
2020-07-27 18:37:17 -0500

    dice game

--- dice1.py
+++ dice1.py
@@ -1,16 +1,21 @@
 import random
 
 dice = [0, 0, 0, 0, 0]
+x = input ("click enter to start the game, or type exit to leave the game ")
 
-for i in range (5):
-    dice[i] = random.randint(1, 6)
+while x != "exit":
+    
+    for i in range (5):
+        dice[i] = random.randint(1, 6)
 
-print("you got", dice)
-dice.sort()
+    print("You got", dice)
+    dice.sort()
 
-if dice[0] == dice[4]:
-    print("yatzee! ")
-elif (dice[0] == dice[3]) or (dice[1] == dice[4]):
-    print("four of a kind")
-else:
-    print("you lost")
+    if dice[0] == dice[4]:
+        print("yatzee! ")
+    elif (dice[0] == dice[3]) or (dice[1] == dice[4]):
+        print("four of a kind")
+    else:
+        print("you lost")
+    x = input ("type exit to leave the game, or click enter to play again ")
+    
