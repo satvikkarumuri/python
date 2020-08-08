dadf8683b899ab209c9ce9c3ddac0cd47ca94167
Satvik.Karumuri <Kalyan.Karumuri@footlocker.com>
2020-08-08 13:47:57 -0500

    working

--- hangman.py
+++ hangman.py
@@ -1,12 +1,48 @@
 import random
 
+# ------------------ VARIABLES -------------------------------
+
 # List of words in the game
-secret_words = ["Cat", "Dog", "Fish", "Parrot", "Hamster", "Frog"]
+secret_words = ["cat", "dog", "fish", "parrot", "hamster", "frog"]
 
 # Create empty list for answer
 answer = []
-# ----------------------------
+# -------------------- END VARIABLES -----------------------------
 
+
+# ------------------ FUNCTIONS -------------------------------
+def setDisplay():
+    # Fill answer with '_'
+    for letter in range (len(secret)):
+        answer.append("_")
+    print(answer)
+
+
+
+# def setBlankValue(position, letter):
+#     answer[position] = letter
+
+def setBlankValue(letter):
+    index = secret.find(letter)
+    if index < 0:
+        print("that is wrong, try again ")
+        exit()
+    else:
+        answer [index] = letter
+
+
+def fillAnswer ():
+    y = 0
+    for x in answer:
+        guess = input("guess a letter A through Z: ")
+        setBlankValue(guess)
+        y = y + 1
+        print (answer)
+
+# ------------------END FUNCTIONS -------------------------------
+
+
+# ------------------------ MAIN LOGIC -------------------------
 # Shuffle words
 random.shuffle(secret_words)
 
@@ -14,17 +50,8 @@ random.shuffle(secret_words)
 secret = secret_words[0]
 
 # Player starts
-# Fill answer with '_'
-for letter in range (len(secret)):
-    letter = answer.append("_")
-print(answer)
-
-y = 0
-for x in answer:
-    guess = input("guess a letter A through Z: ")
-    answer[y] = guess
-    y = y + 1
-    print (answer)
+setDisplay()
+fillAnswer()
 
 # list to string conversion
 answer_str = ""
@@ -35,3 +62,4 @@ if answer_str.lower() == secret.lower():
     print("You did it, the word is", ''.join(secret), "!")
 else:
     print("That is wrong, try again ")
+# ------------------------ END MAIN LOGIC -------------------------
